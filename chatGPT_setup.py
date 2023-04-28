ef27d49021edde04569f2c634f6c7ac5946e6449
selineyang <selineyangyt@gmail.com>
2023-04-27 23:21:37 -0700

    add files from prompt learning material from deeplearning.ai

---
+++ chatGPT_setup.py
@@ -0,0 +1,16 @@
+import openai
+import os
+import json
+
+openai.api_key = $OPENAI_API_KEY
+
+def get_completion(prompt, model = "gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+
