95e8526f3647678c9f38ccc9fd9da3d134c333de
selineyang <yutongyang10@gmail.com>
2023-05-02 18:32:03 -0700

    set API environment variable

--- chatGPT_setup.py
+++ chatGPT_setup.py
@@ -2,7 +2,6 @@ import openai
 import os
 import json
 
-openai.api_key = $OPENAI_API_KEY
 
 def get_completion(prompt, model = "gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
